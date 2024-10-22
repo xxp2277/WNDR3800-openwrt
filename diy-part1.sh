6cdb67636e171e7b634e76884842ca4532db1fb1
xxp2277 <60702545+xxp2277@users.noreply.github.com>
2024-10-22 09:11:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,6 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+apt-get update
+apt-get upgrade
 sudo apt-get install python
